18e4fe8c83ae8a8e3317e88b11908c3aa1a6ebb7
mike <mikerv73@gmail.com>
2024-03-26 17:17:48 +0000

    modify app.js

--- app.js
+++ app.js
@@ -5,25 +5,20 @@ const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 const session = require("express-session");
 const methodOverride = require("method-override");
-
 const homeRouter = require("./routes/home");
 const postsRouter = require("./routes/posts");
 const sessionsRouter = require("./routes/sessions");
 const usersRouter = require("./routes/users");
-
 const app = express();
-
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "hbs");
-
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 app.use(methodOverride("_method"));
-
 app.use(
   session({
     key: "user_sid",
@@ -35,7 +30,6 @@ app.use(
     },
   })
 );
-
 // clear the cookies after user logs out
 app.use((req, res, next) => {
   if (req.cookies.user_sid && !req.session.user) {
@@ -43,7 +37,6 @@ app.use((req, res, next) => {
   }
   next();
 });
-
 // middleware function to check for logged-in users
 const sessionChecker = (req, res, next) => {
   if (!req.session.user && !req.cookies.user_sid) {
@@ -52,27 +45,26 @@ const sessionChecker = (req, res, next) => {
     next();
   }
 };
-
 // route setup
 app.use("/", homeRouter);
 app.use("/posts", sessionChecker, postsRouter);
 app.use("/sessions", sessionsRouter);
 app.use("/users", usersRouter);
-
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
   next(createError(404));
 });
-
 // error handler
 app.use((err, req, res) => {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get("env") === "development" ? err : {};
-
   // render the error page
   res.status(err.status || 500);
   res.render("error");
 });
-
+const PORT = process.env.PORT || 3000; // Default port is 3000
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
 module.exports = app;
