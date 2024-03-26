fd4c49a2cb7d10f562034c53d4d5ec57fd307827
mike <mikerv73@gmail.com>
2024-03-26 12:59:44 +0000

    modify appsec.yml 3

---
+++ scripts/application_start.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# This is a simple application start script for a Node.js application
+echo "Running application start script"
+echo "Starting application"
+cd /var/www/myapp
+npm start
---
+++ scripts/before_install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# This is a simple before install script for a Node.js application
+echo "Running before install script"
+echo "Updating package list"
+sudo apt-get update
