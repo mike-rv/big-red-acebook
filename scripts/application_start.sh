53cb5fb7c5367f86b37faed0b7a2b17c1655176a
mike <mikerv73@gmail.com>
2024-03-26 16:37:43 +0000

    modify appsec.yml 16

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # This is a simple application start script for a Node.js application
 echo "Running application start script"
-echo "Starting application"
+echo "Starting application with PM2"
 cd /var/www/myapp
 
-# Start the application in the background
-nohup npm start > /dev/null 2>&1 &
+# Use PM2 to start the application
+pm2 start ./bin/www --name myapp
