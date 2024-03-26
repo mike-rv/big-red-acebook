97395713a1df6dc8d25a59eb60774f6fda00d0a7
mike <mikerv73@gmail.com>
2024-03-26 17:47:42 +0000

    modify app start sh

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 # This is a simple application start script for a Node.js application
 echo "Running application start script"
-echo "Starting application with PM2"
+echo "Restarting application with PM2"
 cd /var/www/myapp
 
-# Use PM2 to start the application
-pm2 start ./bin/www --name myapp -f
-
+# Restart the application with PM2
+pm2 restart myapp || pm2 start ./bin/www --name myapp
