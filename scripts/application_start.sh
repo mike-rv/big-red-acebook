dfba05047fd54a0b1de57715ebf685749859e55d
mike <mikerv73@gmail.com>
2024-03-27 13:37:54 +0000

    modify before and after install

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
-# This is a simple application start script for a Node.js application
+# Ensuring PM2 is installed
+which pm2 > /dev/null
+if [ $? -ne 0 ]; then
+    echo "PM2 not found. Installing PM2 globally..."
+    npm install pm2 -g
+fi
+
 echo "Running application start script"
 echo "Restarting application with PM2"
 cd /var/www/myapp
 
-pm2 start ./bin/www --name myapp
+# Using pm2 restart to handle both stopped and already running applications
+pm2 restart ./bin/www --name myapp || pm2 start ./bin/www --name myapp
